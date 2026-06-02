dfaa81d9236b6e31acfcf2f18002f62cc3ff98ec
Vladyslav <saccharcrpt@gmail.com>
2026-06-02 22:44:34 +0200

    Fix serializers, URL configuration and code style issues

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,23 @@
-# write urls here
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import (
+    ActorViewSet,
+    CinemaHallViewSet,
+    GenreViewSet,
+    MovieSessionViewSet,
+    MovieViewSet,
+)
+
+router = DefaultRouter()
+router.register("genres", GenreViewSet, basename="genre")
+router.register("actors", ActorViewSet, basename="actor")
+router.register("cinema_halls", CinemaHallViewSet, basename="cinemahall")
+router.register("movies", MovieViewSet, basename="movie")
+router.register("movie_sessions", MovieSessionViewSet, basename="moviesession")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "cinema"
